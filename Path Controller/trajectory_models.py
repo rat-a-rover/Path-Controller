4abc90d74f5de2b61eee51afaa5d7600bf86fd46
Rathin Shah <106177563+rat-a-rover@users.noreply.github.com>
2023-02-13 16:19:06 -0500

    Working in sim

--- Path Controller/trajectory_models.py
+++ Path Controller/trajectory_models.py
@@ -1,17 +1,33 @@
-#!/usr/bin/env python
-
 import math
 import numpy as np
 import numpy.linalg as la
-from planning_controls_modules.control_utils import normalize_mpipi, dist,\
-                                                    __NULL_RADIUS__,\
-                                                    __INFINITE_RADIUS__,\
-                                                    __NULL_ANGLE__
+# from planning_controls_modules.control_utils import normalize_mpipi, dist,\
+#                                                     __NULL_RADIUS__,\
+#                                                     __INFINITE_RADIUS__,\
+#                                                     __NULL_ANGLE__
+
+
+
+__NULL_RADIUS__ = 0.0
+__INFINITE_RADIUS__ = 100
+__NULL_ANGLE__ = math.pi/2
+
+
+def normalize_mpipi(ang):
+    tol = 1e-2
+    norm_ang =  ang % (2*math.pi)
+    norm_ang = (norm_ang + 2*math.pi) % (2*math.pi)
+    if (norm_ang > math.pi + tol):
+        norm_ang -= (2*math.pi) 
+    return norm_ang
+
+def dist(A, B):
+    return math.sqrt((B[0] - A[0])**2 + (B[1] - A[1])**2)
 
 def arc_locate(radius, angle, velocity, side, icoords):
     ocoords = dict()
 
-    for key, icoord in icoords.iteritems():
+    for key, icoord in icoords.items():
         ocoords[key] = {
             'x' : (- radius * math.sin((angle - __NULL_ANGLE__))            + \
                      radius * math.sin((angle - __NULL_ANGLE__)             + \
@@ -42,7 +58,7 @@ def arc_project(radius, angle, velocity, side, icoords):
     #print('origin')
     #print(origin)
 
-    for key, icoord in icoords.iteritems():
+    for key, icoord in icoords.items():
         vector = {
             'x' : icoord['x'] - origin['x'],
             'y' : icoord['y'] - origin['y'],
@@ -105,7 +121,7 @@ def arc_project(radius, angle, velocity, side, icoords):
 def straight_locate(radius, angle, velocity, side, icoords):
     ocoords = dict()
 
-    for key, icoord in icoords.iteritems():
+    for key, icoord in icoords.items():
         ocoords[key] = {
             'x' : velocity * icoord['t'],
             'y' : 0.0,
@@ -117,7 +133,7 @@ def straight_locate(radius, angle, velocity, side, icoords):
 def straight_project(radius, angle, velocity, side, icoords):
     ocoords = dict()
 
-    for key, icoord in icoords.iteritems():
+    for key, icoord in icoords.items():
         ocoords[key] = {
             'x' : icoord['x'],
             'y' : 0.0,
@@ -128,7 +144,7 @@ def straight_project(radius, angle, velocity, side, icoords):
 
 def point_locate(radius, angle, velocity, side, icoords):
     ocoords = dict()
-    for key, icoord in icoords.iteritems():
+    for key, icoord in icoords.items():
         ocoords[key] = {
             'x' : 0.0,
             'y' : 0.0,
@@ -143,7 +159,7 @@ def point_locate(radius, angle, velocity, side, icoords):
 
 def point_project(radius, angle, velocity, side, icoords):
     ocoords = dict()
-    for key, icoord in icoords.iteritems():
+    for key, icoord in icoords.items():
         ocoords[key] = {
             'x' : 0.0,
             'y' : 0.0,
@@ -155,7 +171,7 @@ def point_project(radius, angle, velocity, side, icoords):
 def crab_locate(radius, angle, velocity, side, icoords):
     ocoords = dict()
 
-    for key, icoord in icoords.iteritems():
+    for key, icoord in icoords.items():
         ocoords[key] = {
             'x' : velocity                                      * \
                   math.cos((angle - __NULL_ANGLE__)             * \
@@ -173,7 +189,7 @@ def crab_locate(radius, angle, velocity, side, icoords):
 def crab_project(radius, angle, velocity, side, icoords):
     ocoords = dict()
 
-    for key, icoord in icoords.iteritems():
+    for key, icoord in icoords.items():
         modulus = math.cos(angle - __NULL_ANGLE__) * icoord['x']   + \
                   math.sin(angle - __NULL_ANGLE__) * icoord['y']
 
@@ -192,21 +208,21 @@ def crab_project(radius, angle, velocity, side, icoords):
 
 def model(mtime, mparams):
     def model_handler(t, x, y, O, v):
-        ocoords_locate = LOCATE[mparams['type']](
-            mparams['radius'],
-            mparams['angle'],
-            mparams['velocity'],
-            mparams['side'],
-            { 'rover' : { 't' : t }}
-        )
-        xd = ocoords_locate['rover']['x']
-        yd = ocoords_locate['rover']['y']
-        Od = ocoords_locate['rover']['O']
-        vd = mparams['velocity']
+        # ocoords_locate = LOCATE[mparams['type']](
+        #     mparams['radius'],
+        #     mparams['heading'],
+        #     mparams['velocity'],
+        #     mparams['side'],
+        #     { 'rover' : { 't' : t }}
+        # )
+        # xd = ocoords_locate['rover']['x']
+        # yd = ocoords_locate['rover']['y']
+        # Od = ocoords_locate['rover']['O']
+        # vd = mparams['velocity']
 
         ocoords_project = PROJECT[mparams['type']](
             mparams['radius'],
-            mparams['angle'],
+            mparams['heading'],
             mparams['velocity'],
             mparams['side'],
             { 'rover' : { 't' : t, 'x' : x, 'y' : y , 'O': O}}
@@ -215,8 +231,8 @@ def model(mtime, mparams):
         yp = ocoords_project['rover']['y']
         Op = ocoords_project['rover']['O']
 
-        return xd, yd, Od, vd, xp, yp, Op
-
+        return  xp, yp, Op
+    print("Hello ROS2")
     return model_handler
 
 LOCATE = {
